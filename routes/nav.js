3686e6bb95c27b0d5af7476e8e9c8019e9366ac7
David Nunez <david.nunez@yodle.com>
2019-04-17 17:44:57 -0400

    updated death valley section, updated homepage to reflect that site is under construction

--- routes/nav.js
+++ routes/nav.js
@@ -1,21 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/about', (req, res) => {
-  res.render('about.html')
-});
+// router.get('/about', (req, res) => {
+//   res.render('about.html')
+// });
 router.get('/events', (req, res) => {
   res.render('events.html')
 });
-router.get('/construction', (req, res) => {
-  res.render('construction.html')
+router.get('/death-valley', (req, res) => {
+  res.render('death-valley.html')
 });
-router.get('/interiors', (req, res) => {
-  res.render('interiors.html')
-});
-router.get('/travel', (req, res) => {
-  res.render('travel.html')
-});
+// router.get('/construction', (req, res) => {
+//   res.render('construction.html')
+// });
+// router.get('/interiors', (req, res) => {
+//   res.render('interiors.html')
+// });
+// router.get('/travel', (req, res) => {
+//   res.render('travel.html')
+// });
 // router.get('/landscape', (req, res) => {
 //   res.render('landscape.html')
 // });
