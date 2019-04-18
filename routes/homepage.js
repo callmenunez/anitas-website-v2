e00c5666c482debe4b96258affe87af652d6a5fd
David Nunez <david.nunez@yodle.com>
2019-04-18 02:02:14 -0400

    updated layout on homepage, added new sections

--- routes/homepage.js
+++ routes/homepage.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('home.html')
-});
-
 router.get('/events', (req, res) => {
   res.render('events.html')
 });
 
-router.get('/environments', (req, res) => {
-  res.render('construction.html')
+router.get('/death-valley', (req, res) => {
+  res.render('death-valley.html')
 });
 
-router.get('/travel', (req, res) => {
-  res.render('travel.html')
+router.get('/interiors', (req, res) => {
+  res.render('interiors.html')
+});
+
+router.get('/construction', (req, res) => {
+  res.render('construction.html')
 });
 
 module.exports = router;
--- routes/nav.js
+++ routes/nav.js
@@ -4,18 +4,21 @@ const router = express.Router();
 // router.get('/about', (req, res) => {
 //   res.render('about.html')
 // });
+router.get('/', (req, res) => {
+  res.render('home.html')
+});
 router.get('/events', (req, res) => {
   res.render('events.html')
 });
 router.get('/death-valley', (req, res) => {
   res.render('death-valley.html')
 });
-// router.get('/construction', (req, res) => {
-//   res.render('construction.html')
-// });
-// router.get('/interiors', (req, res) => {
-//   res.render('interiors.html')
-// });
+router.get('/interiors', (req, res) => {
+  res.render('interiors.html')
+});
+router.get('/construction', (req, res) => {
+  res.render('construction.html')
+});
 // router.get('/travel', (req, res) => {
 //   res.render('travel.html')
 // });
